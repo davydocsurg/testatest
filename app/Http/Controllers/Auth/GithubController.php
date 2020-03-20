5321aaa50ac0bcd84e3a5dc3e191ada7d646a538
davydocsurg <davydocsurg@gmail.com>
2020-03-20 10:06:33 -0400

    okkkk

--- app/Http/Controllers/Auth/GithubController.php
+++ app/Http/Controllers/Auth/GithubController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\User;
+use Exception;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
-use App\User;
 
 class GithubController extends Controller
 {
